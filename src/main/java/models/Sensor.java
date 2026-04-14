73521b6301978b549ef641ffae543bbbbdfaaf65
VidathVi <dassanayakevidath@gmail.com>
2026-04-14 19:25:03 +0530

    Initialization

--- src/main/java/models/Sensor.java
+++ src/main/java/models/Sensor.java
@@ -9,5 +9,60 @@ package models;
  * @author dassa
  */
 public class Sensor {
+    private String id;
+    private String type;
+    private String status;
+    private double currentValue;
+    private String roomID;
+
+    public Sensor(String id, String type, String status, double currentValue, String roomID) {
+        this.id = id;
+        this.type = type;
+        this.status = status;
+        this.currentValue = currentValue;
+        this.roomID = roomID;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(double currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public String getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(String roomID) {
+        this.roomID = roomID;
+    }
+    
+    
     
 }
--- src/main/java/models/SensorReading.java
+++ src/main/java/models/SensorReading.java
@@ -9,5 +9,40 @@ package models;
  * @author dassa
  */
 public class SensorReading {
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+    private String id;
+    private long timestamp;
+    private double value;
+
+    public SensorReading(String id, long timestamp, double value) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+    
+    
     
 }
