c4c638655794542ef568ee9cf0db49bf792cbdea
VidathVi <dassanayakevidath@gmail.com>
2026-04-18 13:05:26 +0530

    Update RoomResource.java

--- src/main/java/com/smartcampus/resources/RoomResource.java
+++ src/main/java/com/smartcampus/resources/RoomResource.java
@@ -4,10 +4,26 @@
  */
 package com.smartcampus.resources;
 
+import com.smartcampus.datastore.DataStore;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author dassa
  */
+@Path("/rooms")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class RoomResource {
+    
+    @GET
+    public Response getAllRooms(){
+        return Response.ok(DataStore.rooms.values()).build();
+    } 
     
 }
