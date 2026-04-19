4f570295dd5f5c3da0ed4316f65922f7df9dc310
VidathVi <dassanayakevidath@gmail.com>
2026-04-19 15:04:11 +0530

    SEASALT

---
+++ src/main/java/com/smartcampus/exceptions/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.exceptions;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author dassa
+ */
+@Provider
+public class LinkedResourceNotFoundExceptionMapper implements ExceptionMapper<LinkedResourceNotFoundException> {
+
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException e) {
+        
+        String errorMessage = "{\"error\": \"" + e.getMessage() + "\"}";
+        
+        return Response.status(422).entity(errorMessage).type(MediaType.APPLICATION_JSON).build();
+    }
+    
+}
