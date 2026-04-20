fdc6eb7e60276bc544265b267ac862c2b8f122d1
VidathVi <dassanayakevidath@gmail.com>
2026-04-20 17:27:06 +0530

    Update SensorReadingResource.java

--- src/main/java/com/smartcampus/resources/SensorReadingResource.java
+++ src/main/java/com/smartcampus/resources/SensorReadingResource.java
@@ -4,10 +4,28 @@
  */
 package com.smartcampus.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author dassa
  */
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SensorReadingResource {
     
+    private String sensorId;
+
+    public SensorReadingResource(String sensorId) {
+        this.sensorId = sensorId;
+    }
+    
+    @GET
+    public Response getReading(){
+        
+    }
 }
