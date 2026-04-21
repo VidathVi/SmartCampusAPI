5d208adeac2544f64aa1a3ee1c3fae017f51bb25
VidathVi <dassanayakevidath@gmail.com>
2026-04-21 23:16:35 +0530

    Update SensorReadingResource.java

--- src/main/java/com/smartcampus/resources/SensorReadingResource.java
+++ src/main/java/com/smartcampus/resources/SensorReadingResource.java
@@ -5,10 +5,13 @@
 package com.smartcampus.resources;
 
 import com.smartcampus.datastore.DataStore;
+import com.smartcampus.models.Sensor;
 import com.smartcampus.models.SensorReading;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -31,6 +34,33 @@ public class SensorReadingResource {
     public Response getReading(){
         
         //Fetching all the readings of the current sensor
-        List<SensorReading> sensorReading = DataStore.readings.get(sensorId);
+        List<SensorReading> sensorReadings = DataStore.readings.get(this.sensorId);
+        
+        //make sure the out put is [] instead of null, if sensorReading is empty
+        if(sensorReadings == null){
+            sensorReadings = new ArrayList<>();
+        }
+        
+        
+        return Response.ok(sensorReadings).build();
+    }
+    
+    @POST
+    public Response addSensorReading(SensorReading newReading){
+        
+        Sensor parentSensor = DataStore.sensors.get(this.sensorId);
+        
+        parentSensor.setCurrentValue(newReading.getValue());
+        
+        
+        List<SensorReading> sensorHistory = DataStore.readings.get(this.sensorId);
+        if(sensorHistory == null){
+            sensorHistory = new ArrayList<>();
+            DataStore.readings.put(this.sensorId, sensorHistory);
+        }
+        
+        sensorHistory.add(newReading);
+        
+        return Response.status(Response.Status.CREATED).entity(newReading).build();
     }
 }
