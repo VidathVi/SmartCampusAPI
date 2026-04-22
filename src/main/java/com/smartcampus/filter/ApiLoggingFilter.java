d40b928f2f169e3c2af53c210e92c36c1d2c41eb
VidathVi <dassanayakevidath@gmail.com>
2026-04-22 19:00:42 +0530

    Update ApiLoggingFilter.java

--- src/main/java/com/smartcampus/filter/ApiLoggingFilter.java
+++ src/main/java/com/smartcampus/filter/ApiLoggingFilter.java
@@ -4,10 +4,38 @@
  */
 package com.smartcampus.filter;
 
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
 /**
  *
  * @author dassa
  */
-public class ApiLoggingFilter {
+@Provider
+public class ApiLoggingFilter implements ContainerRequestFilter, ContainerResponseFilter{
+
+    private static final java.util.logging.Logger LOGGER = java.util.logging.Logger.getLogger(ApiLoggingFilter.class.getName());
+    
+    @Override
+    public void filter(ContainerRequestContext crqc) throws IOException {
+        
+        String method = crqc.getMethod();
+        String uri = crqc.getUriInfo().getRequestUri().toString();
+        
+        LOGGER.info("Incoming Requests: " + method + " " + uri);
+        
+    }
+
+    @Override
+    public void filter(ContainerRequestContext crqc, ContainerResponseContext crsc) throws IOException {
+        
+        int status = crsc.getStatus();
+        
+        LOGGER.info("Outgoing Response Status: " + status);
+    }
     
 }
