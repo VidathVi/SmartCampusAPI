d5380bbbe6ec2651cdb695c89d176721f57a9c8c
VidathVi <dassanayakevidath@gmail.com>
2026-04-22 18:21:30 +0530

    Update GlobalExceptionMapper.java

--- src/main/java/com/smartcampus/exceptions/GlobalExceptionMapper.java
+++ src/main/java/com/smartcampus/exceptions/GlobalExceptionMapper.java
@@ -4,6 +4,9 @@
  */
 package com.smartcampus.exceptions;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -17,7 +20,19 @@ public class GlobalExceptionMapper implements ExceptionMapper<Throwable>{
 
     @Override
     public Response toResponse(Throwable e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
+        //Logging the errors that the user cannot see
+        e.printStackTrace();
+        
+        Map<String, Object> errorMessage = new HashMap<>();
+        errorMessage.put("Status", 500);
+        errorMessage.put("Error", "Internal Server Error");
+        errorMessage.put("message", e.getMessage());
+        
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(errorMessage)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
     }
     
 }
